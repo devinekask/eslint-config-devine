1d2baaf13be0927668e6a43468032359feda644e
duivvv <geoffrey.dhuyvetters@gmail.com>
2016-10-17 11:24:12 +0200

    intial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = {
+
+  extends: [
+    `eslint:recommended`
+  ],
+
+  parser: `babel-eslint`,
+
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+    mocha: true,
+    es6: true
+  },
+
+  plugins: [
+    `babel`
+  ],
+
+  rules: {
+
+    indent: [`error`, 2],
+    quotes: [`error`, `backtick`],
+    semi: [`error`, `always`],
+    "prefer-const": `error`,
+    "space-infix-ops": `error`,
+    "quote-props": [`as-needed`, `error`],
+    "no-console": `off`,
+    "prefer-template": `error`,
+    "no-array-constructor": `error`,
+    "no-new-object": `error`,
+    "no-var": `error`,
+    eqeqeq: [`error`, `smart`],
+    "jsx-quotes": [`error`, `prefer-single`],
+    camelcase: `error`,
+    "arrow-spacing": `error`,
+    "object-curly-spacing": [`off`, `never`],
+    "arrow-parens": [`error`, `as-needed`],
+    "key-spacing": `error`,
+    "comma-spacing": [`error`, { before: false, after: true }],
+    "comma-style": [`error`, `last`],
+
+    "babel/object-curly-spacing": [`error`, `never`]
+
+  }
+
+};
---
+++ react.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+  extends: [
+    require.resolve(`./index`),
+    `plugin:react/recommended`,
+  ],
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+
+  plugins: [
+    `react`,
+  ],
+
+  rules: {
+
+    "react/jsx-key": `error`,
+    "react/display-name": `off`,
+    "react/prefer-stateless-function": `error`,
+    "react/jsx-wrap-multilines": `error`,
+    "react/no-string-refs": `error`,
+    "react/jsx-pascal-case": `error`,
+    "react/jsx-curly-spacing": `error`,
+    "react/jsx-no-bind": `error`,
+    "react/jsx-equals-spacing": `error`,
+    "react/jsx-handler-names": `error`,
+    "react/jsx-space-before-closing": [`error`, `always`]
+
+  },
+
+};
