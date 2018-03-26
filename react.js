ac440742bbc572b15a1fba21171da400d6e499ad
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2018-03-26 13:51:00 +0200

    react-app linting

--- react.js
+++ react.js
@@ -1,40 +1,7 @@
 module.exports = {
 
   extends: [
-    require.resolve(`./index`),
-    `plugin:react/recommended`,
+    `react-app`
   ],
-
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-  },
-
-  plugins: [
-    `react`,
-  ],
-
-  rules: {
-
-    "react/jsx-key": `error`,
-    "react/display-name": `off`,
-    "react/prefer-stateless-function": `error`,
-    "react/no-multi-comp": `error`,
-    "react/no-array-index-key": `error`,
-    "react/no-unescaped-entities": `error`,
-    "react/no-unused-prop-types": `error`,
-    "react/no-unknown-property": `error`,
-    "react/require-default-props": `error`,
-    "react/jsx-wrap-multilines": `error`,
-    "react/no-string-refs": `error`,
-    "react/jsx-pascal-case": `error`,
-    "react/jsx-filename-extension": `error`,
-    "react/jsx-curly-spacing": `error`,
-    "react/jsx-equals-spacing": `error`,
-    "react/jsx-handler-names": `error`,
-    "react/jsx-space-before-closing": [`error`, `always`],
-
-  },
 
 };
