7b9ab4560fa4a4e7930495f926d7638a9364adc0
Simon Vanherweghe <simon.vanherweghe@gmail.com>
2018-11-04 11:28:46 +0100

    prefer single quotes, allow template literals

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 module.exports = {
+  extends: ['eslint:recommended'],
 
-  extends: [
-    `eslint:recommended`,
-  ],
-
-  parser: `babel-eslint`,
+  parser: 'babel-eslint',
 
   env: {
     browser: true,
@@ -14,43 +11,44 @@ module.exports = {
     es6: true,
   },
 
-  plugins: [
-    `babel`,
-  ],
+  plugins: ['babel'],
 
   rules: {
+    indent: ['error', 2],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'always'],
+    'prefer-const': 'error',
+    'space-infix-ops': 'error',
+    'quote-props': ['error', 'as-needed'],
+    'no-console': 'off',
+    'prefer-template': 'error',
+    'no-array-constructor': 'error',
+    'no-new-object': 'error',
+    'no-unused-vars': [
+      'error',
+      {
+        ignoreRestSiblings: true,
+      },
+    ],
+    'no-var': 'error',
+    eqeqeq: ['error', 'smart'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    camelcase: 'error',
+    'keyword-spacing': ['error', { before: true, after: true }],
+    'space-in-parens': ['error', 'never'],
+    'space-before-blocks': 'error',
+    'space-unary-ops': [
+      'error',
+      { words: true, nonwords: true, overrides: { '!': false, '.': false } },
+    ],
+    'semi-spacing': ['error', { before: false, after: false }],
+    'arrow-spacing': 'error',
+    'object-curly-spacing': ['off', 'never'],
+    'arrow-parens': ['error', 'as-needed'],
+    'key-spacing': 'error',
+    'comma-spacing': ['error', { before: false, after: true }],
+    'comma-style': ['error', 'last'],
 
-    indent: [`error`, 2],
-    quotes: [`error`, `backtick`],
-    semi: [`error`, `always`],
-    "prefer-const": `error`,
-    "space-infix-ops": `error`,
-    "quote-props": [`error`, `as-needed`],
-    "no-console": `off`,
-    "prefer-template": `error`,
-    "no-array-constructor": `error`,
-    "no-new-object": `error`,
-    "no-unused-vars": [`error`, {
-      ignoreRestSiblings: true,
-    }],
-    "no-var": `error`,
-    eqeqeq: [`error`, `smart`],
-    "jsx-quotes": [`error`, `prefer-single`],
-    camelcase: `error`,
-    "keyword-spacing": [`error`, {before: true, after: true}],
-    "space-in-parens": [`error`, `never`],
-    "space-before-blocks": `error`,
-    "space-unary-ops": [`error`, {words: true, nonwords: true, overrides: {"!": false, ".": false}}],
-    "semi-spacing": [`error`, {before: false, after: false}],
-    "arrow-spacing": `error`,
-    "object-curly-spacing": [`off`, `never`],
-    "arrow-parens": [`error`, `as-needed`],
-    "key-spacing": `error`,
-    "comma-spacing": [`error`, { before: false, after: true }],
-    "comma-style": [`error`, `last`],
-
-    "babel/object-curly-spacing": [`error`, `never`],
-
+    'babel/object-curly-spacing': ['error', 'never'],
   },
-
 };
